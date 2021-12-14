9446202633fe006a71687a46e385df807c607ff9
nancologist <nancologist@gmail.com>
2021-12-14 02:48:24 +0100

    Outsource aws s3

--- src/controllers/article.js
+++ src/controllers/article.js
@@ -1,24 +1,10 @@
 const fs = require('fs');
-const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const s3 = require('../utils/aws-s3');
 
 exports.uploadImage = async (req, res) => {
-  const client = new S3Client({
-    region: 'eu-central-1',
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-    }
-  })
-
-  const command = new PutObjectCommand({
-    Bucket: 'nancologist-blog',
-    Body: fs.createReadStream(req.file.path),
-    Key: req.file.originalname
-  })
-
   let httpCode = 0
   try {
-    const s3Res = await client.send(command)
+    const s3Res = await s3.saveFile(req.file)
     fs.unlinkSync(req.file.path)
     httpCode = s3Res.$metadata.httpStatusCode
     res.status(httpCode).json({
